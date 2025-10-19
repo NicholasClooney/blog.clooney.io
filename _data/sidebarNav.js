b3b251172fffcf6ec24f1ed674aaa995e114b5c2
NicholasClooney <137233165+NicholasClooney@users.noreply.github.com>
2025-10-19 21:29:18 +0100

    Sidebar becomes a menu

---
+++ _data/sidebarNav.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 'blog',
+    label: 'Blog',
+    url: '/',
+  },
+  {
+    id: 'projects',
+    label: 'Projects',
+    url: '/projects/',
+  },
+  {
+    id: 'tags',
+    label: 'Tags',
+    url: '/tags/',
+  },
+  {
+    id: 'about',
+    label: 'About',
+    url: '/about/',
+  },
+];
